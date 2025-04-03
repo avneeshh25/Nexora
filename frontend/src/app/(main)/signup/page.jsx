225ae54c20afe7aa86c9262e98fa830a9963f235
Avneesh <intel15r0@gmail.com>
2025-04-03 21:55:50 +0530

    Update page.jsx

--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -44,9 +44,9 @@ const SignUpPage = () => {
   });
 
   return (
-    <div className="flex h-screen bg-black items-center justify-center bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#a600ff]">
-      <div className="bg-gradient-to-br from-[#1a1a2e] to-[#302b63] p-10 rounded-xl shadow-xl w-full max-w-md text-white">
-        <h2 className="text-2xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-pink-500 to-blue-500">
+    <div className="flex h-screen bg-black items-center justify-center bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#a600ff] p-4 sm:p-0">
+      <div className="bg-gradient-to-br from-[#1a1a2e] to-[#302b63] p-6 sm:p-10 rounded-xl shadow-xl w-full max-w-md text-white">
+        <h2 className="text-2xl sm:text-3xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-pink-500 to-blue-500">
           Create an Account
         </h2>
         <p className="text-gray-400 text-center">Sign up to get started</p>
@@ -152,10 +152,10 @@ const SignUpPage = () => {
             <img src="icons8-google-48.png" alt="Google" />
           </div>
           <div className="bg-black p-4 rounded-full shadow-md cursor-pointer text-white flex items-center justify-center w-12 h-12">
-           <img src="images.png" alt="" />
+            <img src="images.png" alt="" />
           </div>
           <div className="bg-blue-600 p-4 rounded-full shadow-md cursor-pointer text-white flex items-center justify-center w-12 h-12">
-           <img src="Github-desktop-logo-symbol.svg.png" alt="" />
+            <img src="Github-desktop-logo-symbol.svg.png" alt="" />
           </div>
         </div>
 
