7b59adb18cf636a1b9fc583b62e0b33e226cbc7b
shuklalakshya-dev <lakshyashukla31@gmail.com>
2025-05-19 00:02:39 +0530

    .

--- SDK/src/index.ts
+++ SDK/src/index.ts
@@ -1,6 +1,6 @@
 // Export all components and types
 export * from './types';
-export { ApiKeyProvider } from './Context/AppContext';
+export { ApiKeyProvider } from './context/AppContext';
 export { Chat } from './components/Chat';  // Add this line to export the Chat component
 // Add other exports as needed
 
